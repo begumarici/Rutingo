a8a73afa87b0e8cb04dbb5e9a1a1aa4548476960
Begüm Arıcı <aricibegum@gmail.com>
2026-04-21 10:20:18 +0300

    fix: localization capitalization and remove deprecated barStyle

--- Rutingo/Views/Routines/AddRoutineViewController.swift
+++ Rutingo/Views/Routines/AddRoutineViewController.swift
@@ -265,7 +265,6 @@ class AddRoutineViewController: UIViewController {
             deleteButton.isHidden = false
         }
         
-        navigationController?.navigationBar.barStyle = .black
         navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: AppColors.navbarTitle]
     }
     
